16c881bd22d7d1ed5e400b6e248cf8e4ec136f7b
christian <baozaodexianyu@163.com>
2019-10-18 15:00:10 +0800

    change blog server port use 80

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var spawn = require('child_process').spawn
 
-free = spawn('hexo', ['server', '-p 4000'])
+free = spawn('hexo', ['server', '-p 80'])
 
 free.stdout.on('data', function(data) {
   console.log('standard output:\n' + data)
@@ -10,6 +10,6 @@ free.stderr.on('data', function(data) {
   console.log('standard error output:\n' + data)
 })
 
-free.on('exit', function(code, signal) {
+free.on('exit', function(code) {
   console.log('child process eixt ,exit:' + code)
 })
