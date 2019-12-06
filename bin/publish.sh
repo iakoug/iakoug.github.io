f06d991314d4fef131e7ba0069856eada122888c
christian <rollawaypoint@gmail.com>
2019-12-06 17:12:00 +0800

    Fix email

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,8 +2,8 @@
 cd public
 git init
 git add .
-git config user.email "rollwaypoint@gmail.com"
+git config user.email "rollawaypoint@gmail.com"
 git config user.name "christian"
 git remote add origin https://github.com/rollawaypoint/rollawaypoint.github.io.git
-git commit -m 'Site has been automatically published!'
+git commit -m 'Site has been published automatically'
 git push origin HEAD:master -f
