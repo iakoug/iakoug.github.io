b54edf61b4b4186a5fa5890ba7765be5007dcb3c
christian <rollawaypoint@gmail.com>
2019-12-06 15:24:32 +0800

    Make ci

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-gatsby clean
-gatsby build
 cd public
 git init
+git add .
 git config user.email "rollwaypoint@gmail.com"
 git config user.name "christian"
 git remote add origin https://github.com/rollawaypoint/rollawaypoint.github.io.git
-git commit -am 'Site has been automatically published!'
+git commit -m 'Site has been automatically published!'
 git push origin HEAD:master -f
