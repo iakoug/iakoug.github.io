e89e91a82474450f759b34238fc611ceac0bc557
christian <rollawaypoint@gmail.com>
2019-12-06 14:01:09 +0800

    Git identity info

--- bin/publish.sh
+++ bin/publish.sh
@@ -3,6 +3,8 @@ gatsby clean
 gatsby build
 cd public
 git init
+git config user.email "rollwaypoint@gmail.com"
+git config user.name "christian"
 git remote add origin https://github.com/rollawaypoint/rollawaypoint.github.io.git
 git commit -am 'Site has been automatically published!'
 git push origin HEAD:master -f
