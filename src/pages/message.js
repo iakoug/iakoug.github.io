347d2d0129bf90df0f5e86f798b70c213c68b08d
christian <rollawaypoint@gmail.com>
2020-04-12 12:37:34 +0800

    Improve

--- src/pages/message.js
+++ src/pages/message.js
@@ -1,63 +1,81 @@
-import React, { Component } from 'react'
-import Helmet from 'react-helmet'
-import axios from 'axios'
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+import axios from 'axios';
 
-import Layout from '../layout'
-import SEO from '../components/SEO'
-import config from '../../data/SiteConfig'
-import api from '../../data/api'
+import Layout from '../layout';
+import SEO from '../components/SEO';
+import config from '../../data/SiteConfig';
+import api from '../../data/api';
 
 export default class MessageBoard extends Component {
-  state = {
-    name: '',
-    value: '',
-  }
+	state = {
+		name: '',
+		value: ''
+	};
 
-  submit = () => axios.post(api.comment, {
-    name: this.state.name,
-    value: this.state.value,
-  }).then(() => {
-    window.alert('Comment success!')
-    this.setState({
-      name: '',
-      value: ''
-    })
-  }).catch(err => window.alert(err))
+	submit = () => {
+		const { name, value } = this.state;
 
-  handleChange = type => e => this.setState({[type]: e.target.value})
+		if (!name || !value) {
+			return window.alert('Both [name] & [content] must be entered!');
+		}
 
-  render() {
-    return (
-      <Layout>
-        <Helmet title={`Message board – ${config.siteTitle}`} />
-        <SEO />
-        <div className="container message-board">
-          <h2>Message board</h2>
-          <div className="search-container search-container-name">
-            <input
-              className="search"
-              type="text"
-              name="searchTerm"
-              value={this.state.name}
-              placeholder="Type your name..."
-              onChange={this.handleChange('name')}
-            />
-          </div>
-          <div className="search-container search-container-value">
-            <textarea
-              className="search"
-              type="text"
-              name="searchTerm"
-              value={this.state.value}
-              placeholder="Type here to writing something..."
-              onChange={this.handleChange('value')}
-            />
-          </div>
-          <div className="submit-message">
-            <button onClick={this.submit} type="button" className="ant-btn ant-btn-primary" ant-click-animating-without-extra-node="false"><span>Comment</span></button>
-          </div>
-        </div>
-      </Layout>
-    )
-  }
+		return axios
+			.post(api.comment, {
+				name,
+				value
+			})
+			.then(() => {
+				window.alert('Comment success!');
+				this.setState({
+					name: '',
+					value: ''
+				});
+			})
+			.catch(err => window.alert(err));
+	};
+
+	handleChange = type => e => this.setState({ [type]: e.target.value });
+
+	render() {
+		return (
+			<Layout>
+				<Helmet title={`Message board – ${config.siteTitle}`} />
+				<SEO />
+				<div className="container message-board">
+					<h2>Message board</h2>
+					<div className="search-container search-container-name">
+						<input
+							className="search"
+							type="text"
+							name="searchTerm"
+							value={this.state.name}
+							placeholder="Type your name..."
+							onChange={this.handleChange('name')}
+						/>
+					</div>
+					<div className="search-container search-container-value">
+						<textarea
+							className="search"
+							type="text"
+							name="searchTerm"
+							value={this.state.value}
+							placeholder="Type here to writing something..."
+							onChange={this.handleChange('value')}
+						/>
+					</div>
+					<div className="submit-message">
+						<button
+							onClick={this.submit}
+							type="button"
+							className="ant-btn ant-btn-primary"
+							ant-click-animating-without-extra-node="false"
+						>
+							<span>Comment</span>
+						</button>
+					</div>
+				</div>
+			</Layout>
+		);
+	}
 }
