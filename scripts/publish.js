a3d335f3ce7fab2e008785da1e0cb501318fec4b
christian <rollawaypoint@gmail.com>
2021-04-13 11:15:42 +0800

    Update by publish! Tue Apr 13 2021 11:15:42 GMT+0800 (China Standard Time)

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const { execSync } = require("child_process");
 // eslint-disable-next-line import/no-extraneous-dependencies
 const chalk = require("chalk");
@@ -12,24 +13,38 @@ const run = async (cmd) => {
 };
 
 const init = async () => {
+  console.log(chalk.cyan(`\ncd public && touch CNAME\n`));
   await run(`cd public && touch CNAME`);
+  console.log(chalk.cyan(`\ncd public && echo 'kwoks.me' > CNAME\n`));
   await run(`cd public && echo 'kwoks.me' > CNAME`);
+  console.log(chalk.cyan(`\ncd public && git init\n`));
   await run(`cd public && git init`);
+  console.log(chalk.cyan(`\ncd public && git add\n`));
   await run(`cd public && git add .`);
+  console.log(chalk.cyan(`\ncd public && git config user.email\n`));
   await run(`cd public && git config user.email "rollawaypoint@gmail.com"`);
+  console.log(chalk.cyan(`\ncd public && git config user.name\n`));
   await run(`cd public && git config user.name "christian"`);
+  console.log(chalk.cyan(`\ncd public && git remote\n`));
   await run(
     `cd public && git remote add origin https://github.com/justwink/justwink.github.io.git`
   );
+  console.log(chalk.cyan(`\ncd public && git commit\n`));
   await run(
     `cd public && git commit -m 'Site has been published! ${new Date().toString()}'`
   );
+  console.log(chalk.cyan(`\ncd public && git push\n`));
   await run(`cd public && git push origin HEAD:master -f && cd ..`);
+  console.log(chalk.cyan(`\ngit add\n`));
   await run(`git add .`);
+  console.log(chalk.cyan(`\ngit commit\n`));
   await run(`git commit -am 'Update by publish! ${new Date().toString()}'`);
+  console.log(chalk.cyan(`\ngit push\n`));
   await run(`git push`);
 
-  console.log(chalk.green(`\n\nPublish successfully!\n`));
+  console.log(
+    chalk.green(`\n\nPublish successfully!\n${new Date().toString()}`)
+  );
 };
 
 init();
