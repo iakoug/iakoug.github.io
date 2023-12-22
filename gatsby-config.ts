76388524490748d61207a1c3d7fd374a05dcbee6
christian <rollawaypoint@gmail.com>
2023-12-22 15:00:27 +0800

    Fixup

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -150,7 +150,7 @@ export default {
       options: {
         name: config.title,
         short_name: config.title,
-        theme_color: "hsl(31, 92%, 62%)",
+        theme_color: "hsl(220, 100%, 68%)",
         background_color: "hsl(0, 0%, 100%)",
         icon: "content/photo.jpg",
         display: "standalone",
