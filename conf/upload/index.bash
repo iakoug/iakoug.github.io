1eef75a03ed25f95d5d7a9b5438ad37c26eab3eb
christian <rollawaypoint@gmail.com>
2024-04-02 18:31:58 +0800

    New Post

--- conf/upload/index.bash
+++ conf/upload/index.bash
@@ -1,4 +1,5 @@
+
 scp -r ./ root@1.116.219.68:/home/iakoug.cn_nginx
 scp -r ./ root@1.116.219.68:/etc/nginx
-scp -r ./ root@1.116.219.68:/root/christian/blog-tech
+yarn build && cd ./public && scp -r ./ root@1.116.219.68:/root/christian/blog-tech
 scp -r ./ root@1.116.219.68:/root/christian/homepage
